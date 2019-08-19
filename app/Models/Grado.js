e87a616b07e03925f391f97f1221378ec0712fe8
Luis <luis_aguirre_1994@outlook.com>
2019-08-19 18:43:07 -0500

    [add][models] subiendo más modelos

--- app/Models/Grado.js
+++ app/Models/Grado.js
@@ -1,9 +1,12 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const mongoose = use('Mongoose')
 
-class Grado extends Model {
-}
+let schema = mongoose.Schema({
+  grado_Nombre:{type: String,require:true },
+  numero_grado:{type:Number,require:true, max:3},
+}, {
+  timestamps: true
+})
 
-module.exports = Grado
+module.exports = mongoose.model('Grado', schema)

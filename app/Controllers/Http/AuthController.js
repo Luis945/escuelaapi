f9a0803184fa7c97c483e25913c998022c73944d
mavericc <escobarveric@gmail.com>
2019-08-27 00:27:35 -0500

    tipos login

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Alumno = use('App/Models/Alumno');
+const Maistro = use('App/Models/Maestro');
 var jwt = require('jsonwebtoken');
 const Encryption = use('Encryption');
 
@@ -33,23 +34,71 @@ class AuthController {
         const { matricula, curp } = request.all();
 
         try {
-            var data = await Alumno.findOne({
-                Matricula: matricula, 
-                'Datos_secundarios': { $elemMatch: { 'Curp' : curp} }
+
+            var usuario;
+            var tipoUsuario = "";
+            var idAlumno;
+            var alumno;
+            // console.log("alumno")
+            var datat = await Alumno.findOne({
+                Matricula: matricula
             });
+            // console.log("alumno")
+            if (!datat) {
+                usuario = await Alumno.findOne({
+                    'Datos_secundarios': { $elemMatch: { 'Curp' : curp, 'nombre_padre_tutor': matricula} }
+                });
+                console.log(usuario)
+                if (usuario != null) { 
+                    console.log("asignando")
+                    tipoUsuario = "jefesito";
+                    alumno = usuario.Nombre + ' ' + usuario.Apellido_paterno + ' ' + usuario.Apellido_materno;
+                    idAlumno = usuario._id.toString();
+                    console.log(tipoUsuario)
+                }
+                console.log("padre")
+            } else {
+                usuario = await Alumno.findOne({
+                                Matricula: matricula, 
+                                'Datos_secundarios': { $elemMatch: { 'Curp' : curp} }
+                            });
+                if (usuario) { tipoUsuario = "alumno";
+                    alumno = usuario.Nombre + ' ' + usuario.Apellido_paterno + ' ' + usuario.Apellido_materno;
+                    idAlumno = usuario._id.toString(); 
+                }
+                console.log("alumno encontrao")
+            }
 
-            var token = data ? token = jwt.sign({data}, 'LAMEv3') : '';
+            if ( tipoUsuario === "") {
+                console.log("maestro")
+                if (this.rfcValido(matricula)) {
+                    usuario = await Maistro.findOne({
+                        Rfc: matricula,
+                        Telefono: curp
+                    });
 
+                    if(usuario) {tipoUsuario = "profe"}
+                }
+            }
+
+            console.log("okenenne")
+
+            var token = usuario ? token = jwt.sign({usuario}, 'LAMEv3') : '';
+            // console.log(token)
             if (token === '') {
                 throw new Error('Las credenciales no concuerdan con los registros, verifique.');
             }
 
             return response.status(200).json({
-                token: token
+                token: token,
+                tipo: tipoUsuario,
+                alumno: alumno,
+                _id: idAlumno
             });
         } catch (error) {
+            console.log(error);
             return response.status(200).json({
-                error: error,
+                error: error.toString(),
                 status: 401
             });
         }
@@ -76,6 +125,78 @@ class AuthController {
             return false;
         }
 
+    }
+
+    //Función para validar un RFC
+    // Devuelve el RFC sin espacios ni guiones si es correcto
+    // Devuelve false si es inválido
+    // (debe estar en mayúsculas, guiones y espacios intermedios opcionales)
+    rfcValido(rfc, aceptarGenerico = true) {
+        const re = /^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/;
+        var validado = rfc.match(re);
+
+        if (!validado)  //Coincide con el formato general del regex?
+            return false;
+        // console.log(validado)
+        //Separar el dígito verificador del resto del RFC
+        const digitoVerificador = validado.pop(),
+            rfcSinDigito      = validado.slice(1).join(''),
+            len               = rfcSinDigito.length,
+
+        //Obtener el digito esperado
+            diccionario       = "0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ Ñ",
+            indice            = len + 1;
+        var   suma,
+            digitoEsperado;
+
+        if (len == 12) suma = 0
+        else suma = 481; //Ajuste para persona moral
+
+        for(var i=0; i<len; i++)
+            suma += diccionario.indexOf(rfcSinDigito.charAt(i)) * (indice - i);
+        digitoEsperado = 11 - suma % 11;
+        if (digitoEsperado == 11) digitoEsperado = 0;
+        else if (digitoEsperado == 10) digitoEsperado = "A";
+
+        //El dígito verificador coincide con el esperado?
+        // o es un RFC Genérico (ventas a público general)?
+        if ((digitoVerificador != digitoEsperado)
+        && (!aceptarGenerico || rfcSinDigito + digitoVerificador != "XAXX010101000"))
+            return false;
+        else if (!aceptarGenerico && rfcSinDigito + digitoVerificador == "XEXX010101000")
+            return false;
+        // console.log(rfcSinDigito, digitoVerificador)
+        return rfcSinDigito + digitoVerificador;
+    }
+
+    //nombre papá y matrícula
+    //administrativa superusuario
+    //maestro rfc y numero de telefono
+    //Función para validar una CURP
+    curpValida(curp) {
+        var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
+            validado = curp.match(re);
+        
+        if (!validado)  //Coincide con el formato general?
+            return false;
+        
+        //Validar que coincida el dígito verificador
+        function digitoVerificador(curp17) {
+            //Fuente https://consultas.curp.gob.mx/CurpSP/
+            var diccionario  = "0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",
+                lngSuma      = 0.0,
+                lngDigito    = 0.0;
+            for(var i=0; i<17; i++)
+                lngSuma = lngSuma + diccionario.indexOf(curp17.charAt(i)) * (18 - i);
+            lngDigito = 10 - lngSuma % 10;
+            if (lngDigito == 10) return 0;
+            return lngDigito;
+        }
+    
+        if (validado[2] != digitoVerificador(validado[1])) 
+            return false;
+            
+        return true; //Validado
     }
 }
 

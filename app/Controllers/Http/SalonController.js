94a8d6a191c487d5c0dc642c271d036150920fad
Sunset1234 <emmanuel_burciaga@hotmail.com>
2019-08-29 18:05:36 -0500

    Quité unos console log

--- app/Controllers/Http/SalonController.js
+++ app/Controllers/Http/SalonController.js
@@ -9,7 +9,7 @@ class SalonController {
     
     async setSalon ({response,request}) {
       
-        console.log(request.all())
+        //console.log(request.all())
         var {Grado,Seccion,Ciclo,Maestro,Alumnos,Materias}= request.all();
         var salon= new Salon({
           Grado,
@@ -20,14 +20,14 @@ class SalonController {
           Materias
         });
 
-        console.log("-------------------------------------------------------------");
-        console.log("-------------------------------------------------------------");
-        console.log("-------------------------------------------------------------");
-        console.log(salon);
+        //console.log("-------------------------------------------------------------");
+        //console.log("-------------------------------------------------------------");
+        //console.log("-------------------------------------------------------------");
+        //console.log(salon);
 
-        console.log("-------------------------------------------------------------");
-        console.log("-------------------------------------------------------------");
-        console.log("-------------------------------------------------------------");
+        //console.log("-------------------------------------------------------------");
+        //console.log("-------------------------------------------------------------");
+        //console.log("-------------------------------------------------------------");
 
         var _idchido;
 
@@ -55,17 +55,17 @@ class SalonController {
       //se inserta a los alumnos calificaciones en 0 por cada unidad y materia
       //await no jala en un foreach :'( ahi dispensen el desmadre
       for (let i = 0; i < Alumnos.length; i++) {
-        console.log("-------------------------------------------------------------");
-        console.log("ITERANDO EL ARREGLO DE ALUMNOS QUE CONTIENE:");
-        console.log(Alumnos);
-        console.log("ITERANDO EL ARREGLO DE ALUMNOS QUE CONTIENE");
+        //console.log("-------------------------------------------------------------");
+        //console.log("ITERANDO EL ARREGLO DE ALUMNOS QUE CONTIENE:");
+        //console.log(Alumnos);
+        //console.log("ITERANDO EL ARREGLO DE ALUMNOS QUE CONTIENE");
         for (let j = 0; j < materiasAInsertar.length; j++) {
           var calificaciones;
-          console.log("-------------------------------------------------------------");
-        console.log("ITERANDO EL ARREGLO DE MATERIAS QUE CONTIENE:");
-        console.log(materiasAInsertar);
-        console.log("ITERANDO EL ARREGLO DE MATERIAS QUE CONTIENE");
-        console.log("-------------------------------------------------------------");
+        //  console.log("-------------------------------------------------------------");
+        //console.log("ITERANDO EL ARREGLO DE MATERIAS QUE CONTIENE:");
+        //console.log(materiasAInsertar);
+        //console.log("ITERANDO EL ARREGLO DE MATERIAS QUE CONTIENE");
+        //console.log("-------------------------------------------------------------");
           for (let k = 0; k < materiasAInsertar[j].unidades.length; k++) {
             calificaciones = {
               id_materia: materiasAInsertar[j]._id,
@@ -73,16 +73,16 @@ class SalonController {
               calificacion: 0
             };
 
-            console.log("ITERANDO EL ARREGLO DE UNIDADES QUE CONTIENE:");
-          console.log(materiasAInsertar[j].unidades);
-          console.log("ITERANDO EL ARREGLO DE UNIDADES QUE CONTIENE");
-          console.log("-------------------------------------------------------------");
-          console.log("PREVIO A GUARDAR");
-            console.log(calificaciones);
-          console.log("PREVIO A GUARDAR");
-          console.log("-------------------------------------------------------------");
+          //  console.log("ITERANDO EL ARREGLO DE UNIDADES QUE CONTIENE:");
+          //console.log(materiasAInsertar[j].unidades);
+          //console.log("ITERANDO EL ARREGLO DE UNIDADES QUE CONTIENE");
+          //console.log("-------------------------------------------------------------");
+          //console.log("PREVIO A GUARDAR");
+          //  console.log(calificaciones);
+          //console.log("PREVIO A GUARDAR");
+          //console.log("-------------------------------------------------------------");
             await Alumno.findOneAndUpdate({_id: Alumnos[i]._id}, {$push: {calificaciones: calificaciones}});
-            console.log("materia guardada???");
+            //console.log("materia guardada???");
           }
           
         }
@@ -92,7 +92,7 @@ class SalonController {
 
         // await salon.save();
         await salon.save(function (err) {
-          console.log(err)
+          //console.log(err)
           const chatin = new Chat({
             Salon: salon._id,
             Mensajes: [{
@@ -102,7 +102,7 @@ class SalonController {
           });
 
           chatin.save(function (err) {
-            console.log(err)
+            //console.log(err)
             // thats it!
           });
           // saved!
@@ -145,7 +145,7 @@ class SalonController {
       var obtenido;
       await Alumno.find({Status:'sin_asignar'}).exec((error,datos)=>{
         obtenido=datos;
-        console.log(obtenido)
+        //console.log(obtenido)
       })
   
       return response.status(200).json(obtenido);
@@ -155,9 +155,9 @@ class SalonController {
       var alumnos;
       var salon= request.all();
        alumnos = salon.Alumnos;
-      console.log(params.id)
+      //console.log(params.id)
       await Salon.updateOne({_id: salon._id},{ $pullAll: {Alumnos: [params.id] }})
-      console.log('llego aqui')
+      //console.log('llego aqui')
       
       return  response.status(200).json(salon);
 
